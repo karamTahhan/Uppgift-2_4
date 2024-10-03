7b7314e7784fba771fe1127c8c87282c16c1ef57
karamTahhan <kata07001@utb.vaxjo.se>
2024-10-03 14:22:00 +0200

    Add project files.

---
+++ Uppgift 2_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace uppgift2_4
+{
+    class program
+    {
+        static void Main(string[] args) 
+            {
+            Console.WriteLine("vad är lönet för person 1");
+            int P1= int.Parse(Console.ReadLine());
+            Console.WriteLine("vad är löner för person 2");
+            int P2= int.Parse(Console.ReadLine());
+            Console.WriteLine("vad är lönet för person 3");
+            int P3= int.Parse(Console.ReadLine());        
+            int sum = (P1 + P2 + P3) / 3;
+            Console .WriteLine(sum);
+
+
+
+
+
+
+            }
+
+    }
+
+
+
+}
